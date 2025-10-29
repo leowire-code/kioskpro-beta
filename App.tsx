13f472ab32cf9f5e0b70ac6fc5190b401ab32b3f
leowire-code <leandrowire@yahoo.com.br>
2025-10-29 20:33:56 -0300

    feat: versão beta inicial do KioskPro (garçom)

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import RootNavigator from './navigation';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
